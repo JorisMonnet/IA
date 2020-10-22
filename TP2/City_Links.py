61432b4048ed11b7e8fa735fcbdbf5e518cc987b
Monnet Joris <joris.monnet@he-arc.ch>
2020-10-22 09:41:53 +0200

    use dictionnary when reading files and begin to work on astar algorithm

--- TP2/City_Links.py
+++ TP2/City_Links.py
@@ -10,8 +10,32 @@ class City :
 
     def createLinks(self,link):
         self.links.append(link)
+    def getPos(self):
+        return (self.x,self.y)
         
 class Links:
     def __init__(self,destination,weight):
         self.destination=destination
         self.weight=weight
+
+def readCityFile(dicCity):
+    file=open("positions.txt","r")
+    for linePos in file:
+        name,x,y=linePos.split()
+        dicCity[name]=City(name,x,y)
+
+def readLinksFile(dicCity):
+    file=open("connections.txt","r")
+    for lineCon in file:
+        src,dst,weight=lineCon.split()
+        try:
+            dicCity[src].createLinks(Links(dicCity[dst],weight))
+            dicCity[dst].createLinks(Links(dicCity[src],weight))
+        except:
+            print("error in file")
+
+def readAll():
+    dicCity={}
+    readCityFile(dicCity)
+    readLinksFile(dicCity)
+    return dicCity
--- TP2/TP2.py
+++ TP2/TP2.py
@@ -1,21 +1,35 @@
-import City_Links
-def readPositions(listCity):
-    file=open("positions.txt","r")
-    for linePos in file:
-        name,x,y=linePos.split()
-        listCity.append(City_Links.City(name,x,y))
+from City_Links import readAll
+import math
+import heapq
 
-def readConnections(listCity):
-    file=open("connections.txt","r")
-    for lineCon in file:
-        src,dst,weight=lineCon.split()
-        for city in listCity:
-            if src==city.name:
-                city.createLinks(City_Links.Links(dst,weight))
+def h0(n,B): return 0
+def h1(n,B): return B.X-n.X
+def h2(n,B): return B.Y-n.Y
+def h3(n,B): return math.sqrt((B.X-n.X)**2+(B.Y+n.Y)**2)
+def h4(n,B): return h1(n,B)+h2(n,B)
+
+def aStarAlgorithm(cityA,cityB,h,start):
+    iteration=1
+    frontiere=[cityA.getPos()]
+    history=[]
+
+    while frontiere:
+            state = heapq.heappop(frontiere)
+            history[etat] = state.f
+            if state.final(cityB.getPos()):
+                return state
+            for op in state.applicable_operators():
+                new = state.apply(op)
+                new.f = cityBlock(new.values) + new.depth
+                if new not in history or new.f < history[new]:
+                    heapq.heappush(frontiere, new)
+            iteration += 1
+    raise Exception("No Solution")
+    
+
+
 
 if __name__ == "__main__":
-    listCity=[]
-    readPositions(listCity)
-    readConnections(listCity)
-    for city in listCity:
-        print(city)
+    dicCity = readAll()
+
+
